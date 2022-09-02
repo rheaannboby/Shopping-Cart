06ffa6c2a8f4634dc1225e98309c490bc6295412
rheaannboby <ron123rhea@gmail.com>
2022-09-02 10:12:12 +0530

    Added actuator and removed Swagger configuration from cart service

--- cart-service/src/main/java/com/capg/cartservice/config/SwaggerConfig.java
+++ cart-service/src/main/java/com/capg/cartservice/config/SwaggerConfig.java
@@ -3,15 +3,7 @@ package com.capg.cartservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import static springfox.documentation.builders.PathSelectors.regex;
-
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.RequestHandlerSelectors;
-
+/*
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
@@ -23,4 +15,9 @@ public class SwaggerConfig {
 	private ApiInfo apiInfo() {
 		return new ApiInfoBuilder().title("Cart REST API").description("API for cart management").termsOfServiceUrl("http://rhea.com").license("Rhea license").licenseUrl("ron123rhea@gmail.com").version("1.0").build();
 	}
+}
+*/
+
+public class SwaggerConfig {
+	
 }
